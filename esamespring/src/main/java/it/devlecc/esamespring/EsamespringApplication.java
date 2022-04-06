542f8cea5ef93a4bfdc0d930b201f5c8129d90d0
Andrea Torino <andrea.torino244@gmail.com>
2022-04-06 11:34:48 +0200

    Aggiunta del Pom

---
+++ esamespring/src/main/java/it/devlecc/esamespring/EsamespringApplication.java
@@ -0,0 +1,13 @@
+package it.devlecc.esamespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsamespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsamespringApplication.class, args);
+	}
+
+}
