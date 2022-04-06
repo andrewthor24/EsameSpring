99c274423d1d22bf56a082a22b0f94871d5978a0
Andrea Torino <andrea.torino244@gmail.com>
2022-04-06 13:49:57 +0200

    Aggiunta repository

--- esamespring/src/main/java/it/devlecc/esamespring/persistenza/ProdottoRepository.java
+++ esamespring/src/main/java/it/devlecc/esamespring/persistenza/ProdottoRepository.java
@@ -3,5 +3,11 @@ package it.devlecc.esamespring.persistenza;
 import it.devlecc.esamespring.model.Prodotto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ProdottoRepository extends JpaRepository<Prodotto, Long> {
+    List<Prodotto> findByDataAcquistoBetween(Date datada, Date dataa);
+    List<Prodotto> findByPrezzoBetween(float minimo, float max);
+    List<Prodotto> findByPrezzoLessThan(float max);
 }
